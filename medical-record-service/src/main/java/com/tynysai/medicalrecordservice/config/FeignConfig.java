fec975027a8952da72d18112c71d8bf4a8cc1e3d
Bekasyl Asylbekov <basylbekov@crystalspring.kz>
2026-04-21 12:30:08 +0500

    FEA: Добавлен Retryer в Feign-клиенты

---
+++ medical-record-service/src/main/java/com/tynysai/medicalrecordservice/config/FeignConfig.java
@@ -0,0 +1,15 @@
+package com.tynysai.medicalrecordservice.config;
+
+import feign.Retryer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class FeignConfig {
+    @Bean
+    public Retryer feignRetryer() {
+        return new Retryer.Default(100L, TimeUnit.SECONDS.toMillis(1L), 3);
+    }
+}
